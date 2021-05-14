7547faa54e88415cb36f80670546a74c77b30eba
Jason Sy <jasyrotuck@gmail.com>
2021-05-14 12:42:38 -0700

    no booking.booking_type

--- backend/app/data_gen/__init__.py
+++ backend/app/data_gen/__init__.py
@@ -9,12 +9,11 @@ from managers.lottery import LotteryPool
 
 from .booking_factory import BookingFactory
 
-def _genBooking(db, date: date, am_or_pm: bool, booking_type: str, th: Trailhead, batch_size = 1, ):
+def _genBooking(db, date: date, am_or_pm: bool, th: Trailhead, batch_size = 1, ):
     b = BookingFactory.create_batch(size=batch_size,
         date=date,
         am_or_pm=am_or_pm,
-        trailhead_id = th.id,
-        booking_type=booking_type)
+        trailhead_id = th.id)
     db.bulk_save_objects(b)
     db.commit()
     return b
@@ -37,6 +36,6 @@ def gen_bookings_for_lottery_pool(l: LotteryPool, db) -> List[Any]:
     print(l.__dict__)
     while generated_interest < interest_output: 
         print('GENERATING MORE {} < {} ', generated_interest, interest_output)
-        bookings = _genBooking(db, l.date, l.am_or_pm, l.trailhead.capacity_type, l.trailhead, interest_output//8)
+        bookings = _genBooking(db, l.date, l.am_or_pm, l.trailhead, interest_output//8)
         generated_interest += sum([b.num_of_persons for b in bookings])
     return
