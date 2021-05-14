78427f8ec733d79fad5ec3399b93440e6997fce9
justinmacaulay <justinkmacaulay@gmail.com>
2021-05-14 10:35:25 -0700

    enum fixes

--- backend/app/alembic/versions/32ebe93bb91b_trying_an_enum_for_state.py
+++ backend/app/alembic/versions/32ebe93bb91b_trying_an_enum_for_state.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '32ebe93bb91b'
-down_revision = 'bd8fa260b710'
+down_revision = 'ab9562e911dc'
 branch_labels = None
 depends_on = None
 
--- backend/app/data_gen/booking_factory.py
+++ backend/app/data_gen/booking_factory.py
@@ -2,6 +2,7 @@ from random import randint, choice
 
 import factory
 from models.booking import Booking
+from models.model_enums import BookingState
 
 class BookingFactory(factory.Factory):
     class Meta:
@@ -16,4 +17,4 @@ class BookingFactory(factory.Factory):
     num_of_persons = factory.LazyAttribute(lambda x: randint(1,8))
     vehicle_licence_plate = factory.Faker('bothify', text='?#?#?#', letters='ABCEGHJKLMNPRSTVXY')
     application_datetime = factory.Faker('past_datetime', start_date='-30d')
-    state = factory.LazyAttribute(lambda: choice(["WAITING", "STANDBY", "OPT-OUT", "PASS OFFERED", "BOOKING CONFIRMED", "PASS CANCELED"]))
+    state = factory.LazyAttribute(lambda x: choice([state.value for state in BookingState]))
